46d97b8f936c72a0761cf2d598ea294199fabac1
Vladsanin777 <vlad.sanin.2007.09@gmail.com>
2024-01-30 16:57:15 +0400

    saf

---
+++ Urok_3/ex4.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+
+min_1(int a, int b){
+  if(a < b){
+    return a;
+  }else{
+    return b;
+  }
+}
+int main() {
+  int n, k;
+  cin >> n >> k;
+
+  // Находим наибольший общий делитель N и K.
+  int gcd = 1;
+  for (int i = 2; i <= min_1(n, k); i++) {
+    if (n % i == 0 && k % i == 0) {
+      gcd = i;
+    }
+  }
+
+  // Вычисляем искомое количество поворотов.
+  int rotations = n * k / gcd;
+
+  cout << rotations << endl;
+
+  return 0;
+}
